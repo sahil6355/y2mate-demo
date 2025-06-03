46e65479c1220887b7ef53410e8c57433d5948c4
hensithummar <thummarhensi2001@gmail.com>
2025-06-03 16:40:12 +0530

    container setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,79 +5,81 @@ import SeachContainer from "./SeachContainer";
 import { useLocation } from "@reach/router";
 
 const Home = ({ searchProps, convertProps }) => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  const features = t("features", { returnObjects: true });
-  const homeFaqs = t("homeFaqs", { returnObjects: true });
+    const features = t("features", { returnObjects: true });
+    const homeFaqs = t("homeFaqs", { returnObjects: true });
 
-  const location = useLocation();
-  const currentPath = location.pathname; // e.g. "/"
+    const location = useLocation();
+    const currentPath = location.pathname; // e.g. "/"
 
-  const introParagraph = t("introParagraph").replace("{{link}}", currentPath);
+    const introParagraph = t("introParagraph").replace("{{link}}", currentPath);
 
 
-  return (
-    <>
-      <SeachContainer
-        searchLocation={searchProps}
-        convertLocation={convertProps}
-      />
-
-      <h2 className="text-center mt-48">{t("headTitle")}</h2>
-      <p
-        className="text-justify"
-        dangerouslySetInnerHTML={{ __html: introParagraph }}
-      ></p>
-      <p>{t("introParagraph2")}</p>
-      <div className="border-line2 mt-48"></div>
-      <div className="y2meta-guide">
-        <div className="how-to-download">
-          <h2>{t("howToDownload")}</h2>
-          <ol>
-            <li>{t("step1")}</li>
-            <li>{t("step2")}</li>
-            <li>{t("step3")}</li>
-            <li>{t("step4")}</li>
-          </ol>
-        </div>
-        <div className="y2meta-advantages">
-          <h2>{t("advantages")}</h2>
-          <ul>
-            <li>{t("adStep1")}</li>
-            <li>{t("adStep2")}</li>
-            <li>{t("adStep3")}</li>
-            <li>{t("adStep4")}</li>
-            <li>{t("adStep5")}</li>
-          </ul>
-        </div>
-      </div>
-      <div className="border-line2 mt-48"></div>
-      <h2 className="text-center mt-48">{t("featuresHeading")}</h2>
-      <div className="feature-list mt-48">
-        {features?.map?.((feature, index) => (
-          <div className="feature-item" key={index}>
-            <img
-              src={`../../images/${feature?.imgClass}.webp`}
-              width="50"
-              height="50"
-              alt={feature?.title}
+    return (
+        <>
+            <SeachContainer
+                searchLocation={searchProps}
+                convertLocation={convertProps}
             />
-            <h3>{feature.title}</h3>
-            <p>{feature.description}</p>
-          </div>
-        ))}
-      </div>
-      <div className="border-line2 mt-48"></div>
-      <h2 className="text-center mt-48">{t("faqHeading")}</h2>
-      {homeFaqs?.map?.((faq, index) => (
-        <div className="faq-sec" key={index}>
-          <h3>{faq.question}</h3>
-          <p>{faq.answer}</p>
-        </div>
-      ))}
 
-    </>
-  );
+            <div className="container">
+                <h2 className="text-center mt-48">{t("headTitle")}</h2>
+                <p
+                    className="text-justify"
+                    dangerouslySetInnerHTML={{ __html: introParagraph }}
+                ></p>
+                <p>{t("introParagraph2")}</p>
+                <div className="border-line2 mt-48"></div>
+                <div className="y2meta-guide">
+                    <div className="how-to-download">
+                        <h2>{t("howToDownload")}</h2>
+                        <ol>
+                            <li>{t("step1")}</li>
+                            <li>{t("step2")}</li>
+                            <li>{t("step3")}</li>
+                            <li>{t("step4")}</li>
+                        </ol>
+                    </div>
+                    <div className="y2meta-advantages">
+                        <h2>{t("advantages")}</h2>
+                        <ul>
+                            <li>{t("adStep1")}</li>
+                            <li>{t("adStep2")}</li>
+                            <li>{t("adStep3")}</li>
+                            <li>{t("adStep4")}</li>
+                            <li>{t("adStep5")}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="border-line2 mt-48"></div>
+                <h2 className="text-center mt-48">{t("featuresHeading")}</h2>
+                <div className="feature-list mt-48">
+                    {features?.map?.((feature, index) => (
+                        <div className="feature-item" key={index}>
+                            <img
+                                src={`../../images/${feature?.imgClass}.webp`}
+                                width="50"
+                                height="50"
+                                alt={feature?.title}
+                            />
+                            <h3>{feature.title}</h3>
+                            <p>{feature.description}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="border-line2 mt-48"></div>
+                <h2 className="text-center mt-48">{t("faqHeading")}</h2>
+                {homeFaqs?.map?.((faq, index) => (
+                    <div className="faq-sec" key={index}>
+                        <h3>{faq.question}</h3>
+                        <p>{faq.answer}</p>
+                    </div>
+                ))}
+            </div>
+
+        </>
+    );
 };
 
 export default Home;
