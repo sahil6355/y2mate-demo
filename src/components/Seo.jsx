cd3209187791f597548c30493dee70072aae609f
hensithummar <thummarhensi2001@gmail.com>
2025-06-04 15:34:23 +0530

    1

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -35,7 +35,7 @@ const Seo = () => {
             <link rel="preload" fetchpriority="high" as="image" href="/images/logo.webp" type="image/webp" />
             <link rel="icon" href="/images/favicon.webp" />
 
-            <link rel="canonical" href={`https://y2meta.lol${currentLang === "en" ? `${currentPath}` : `${currentPath}`}`} />
+            <link rel="canonical" href={`https://y2meta.lol${currentPath === "en" ? `${currentPath}` : `${currentPath}`}`} />
             
             {languages?.map?.((k) => (
 
