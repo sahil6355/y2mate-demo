6e0ac1f174802858324a24701b1e979b8798f2f6
hensithummar <thummarhensi2001@gmail.com>
2025-06-05 12:09:22 +0530

    1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,81 +1,66 @@
 const { languages, defaultLanguage } = require('./src/constant');
 
 module.exports = {
-    siteMetadata: {
-        title: `yt1`,
-        siteUrl: `https://y2meta.lol`,
+  siteMetadata: {
+    title: `yt1`,
+    siteUrl: `https://y2meta.lol`,
+  },
+  plugins: [
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "static/images/favicon.webp",
+      },
     },
-    plugins: [
-        "gatsby-plugin-sitemap",
-        {
-            resolve: "gatsby-plugin-manifest",
-            options: {
-                icon: "static/images/favicon.webp",
-            },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locales`,
+        languages,
+        defaultLanguage,
+        siteUrl: `https://y2meta.lol`,
+        redirect: true,
+        getLanguageFromPath: true,
+        prefixDefaultLanguage: true,
+        i18nextOptions: {
+          defaultNS: 'translation',
+          //debug: true,
+          lowerCaseLng: true,
+          saveMissing: false,
+          interpolation: {
+            escapeValue: false // not needed for react as it escapes by default
+          },
+          // keySeparator: false,
+          // nsSeparator: false
         },
-        "gatsby-plugin-react-helmet",
-        {
-            resolve: `gatsby-plugin-react-i18next`,
-            options: {
-                localeJsonSourceName: `locales`,
-                languages,
-                defaultLanguage,
-                siteUrl: `https://y2meta.lol`,
-                redirect: true,
-                getLanguageFromPath: true,
-                prefixDefaultLanguage: true,
-                i18nextOptions: {
-                    defaultNS: 'translation',
-                    ns: ['translation', 'youtube-to-mp3', 'youtube-to-mp4'],
-                    //debug: true,
-                    lowerCaseLng: true,
-                    saveMissing: false,
-                    interpolation: {
-                        escapeValue: false // not needed for react as it escapes by default
-                    },
-                    // keySeparator: false,
-                    // nsSeparator: false
-                },
-                pages: [
-                    {
-                        matchPath: '/search',
-                        languages: [defaultLanguage]
-                    },
-                    {
-                        matchPath: '/convert',
-                        languages: [defaultLanguage]
-                    },
-                    {
-                        matchPath: '/about',
-                        languages: [defaultLanguage]
-                    },
-                    {
-                        matchPath: '/contact',
-                        languages: [defaultLanguage]
-                    },
-                ]
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `locales`,
-                path: `${__dirname}/src/locales/lang`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `locales`,
-                path: `${__dirname}/src/locales/youtube-to-mp3`, // mp3-specific texts
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `locales`,
-                path: `${__dirname}/src/locales/youtube-to-mp4`, // mp4-specific texts
-            },
-        },
-    ],
+        pages: [
+          {
+            matchPath: '/search',
+            languages: [defaultLanguage]
+          },
+          {
+            matchPath: '/convert',
+            languages: [defaultLanguage]
+          },
+          {
+            matchPath: '/about',
+            languages: [defaultLanguage]
+          },
+          {
+            matchPath: '/contact',
+            languages: [defaultLanguage]
+          },
+        ]
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `locales`,
+        path: `${__dirname}/src/locales`,
+      },
+    },
+  ],
 };
