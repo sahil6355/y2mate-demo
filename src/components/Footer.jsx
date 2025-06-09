df37bf365b7ba99385bbb30eedbd6fc382ee8d3a
hensithummar <thummarhensi2001@gmail.com>
2025-06-09 15:09:05 +0530

    final code changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,50 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useEffect } from "react";
 
 const Footer = () => {
 
-  useEffect(() => {
-    // Inject Google Analytics script
-    const script1 = document.createElement("script");
-    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-R1QQF1FWWS";
-    script1.async = true;
-    document.head.appendChild(script1);
+    useEffect(() => {
+        // Inject Google Analytics script
+        const script1 = document.createElement("script");
+        script1.src = "https://www.googletagmanager.com/gtag/js?id=G-R1QQF1FWWS";
+        script1.async = true;
+        document.head.appendChild(script1);
 
-    const script2 = document.createElement("script");
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-R1QQF1FWWS');
-    `;
-    document.head.appendChild(script2);
-  }, []);
+        const script2 = document.createElement("script");
+        script2.innerHTML = `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R1QQF1FWWS');
+        `;
+        document.head.appendChild(script2);
+    }, []);
+    
+    return (
+        <>
 
-  return (
-    <>
+            <div className="container">
+                <div className="footer">
+                    <p className="text-center">@2025 y2meta</p>
+                    <ul>
+                        <li>
+                            <Link to="/about">About </Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/terms-condition">Terms and Condition </Link>
+                        </li>
+                        <li>
+                            <Link to="/privacy-policy">Privacy Policy</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
 
-      <div className="container">
-        <div className="footer">
-          <p className="text-center">@2025 y2meta</p>
-          <ul>
-            <li>
-              <Link to="/about">About </Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/terms-condition">Terms and Condition </Link>
-            </li>
-            <li>
-              <Link to="/privacy-policy">Privacy Policy</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
-
-  );
+    );
 };
 
 export default Footer;
