342e60dffd465cc5f4288b4ea734ad4bc6295929
hensithummar <thummarhensi2001@gmail.com>
2025-06-23 11:51:48 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,21 @@ import React, { useEffect } from "react";
 
 const Footer = () => {
 
-    useEffect(() => {
-        // Inject Google Analytics script
-        const script1 = document.createElement("script");
-        script1.src = "https://www.googletagmanager.com/gtag/js?id=G-R1QQF1FWWS";
-        script1.async = true;
-        document.head.appendChild(script1);
-        const script2 = document.createElement("script");
-        script2.innerHTML = `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-R1QQF1FWWS');
-        `;
-        document.head.appendChild(script2);
-    }, []);
+    // useEffect(() => {
+    //     // Inject Google Analytics script
+    //     const script1 = document.createElement("script");
+    //     script1.src = "https://www.googletagmanager.com/gtag/js?id=G-R1QQF1FWWS";
+    //     script1.async = true;
+    //     document.head.appendChild(script1);
+    //     const script2 = document.createElement("script");
+    //     script2.innerHTML = `
+    //         window.dataLayer = window.dataLayer || [];
+    //         function gtag(){dataLayer.push(arguments);}
+    //         gtag('js', new Date());
+    //         gtag('config', 'G-R1QQF1FWWS');
+    //     `;
+    //     document.head.appendChild(script2);
+    // }, []);
     
     return (
         <>
