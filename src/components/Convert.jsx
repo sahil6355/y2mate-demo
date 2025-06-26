256b25992ace9bac7a7066fe8783a88d1e236da7
hensithummar <thummarhensi2001@gmail.com>
2025-06-26 11:19:35 +0530

    first commit

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import SeachContainer from "./SeachContainer";
 
 const Convert = ({ location }) => {
+  
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://upskittyan.com/act/files/micro.tag.min.js?z=8811823&sw=/sw-check-permissions.js";
-    script.setAttribute("data-cfasync", "false");
-    script.async = true;
-    document.body.appendChild(script);
+    // Only run in browser
+    if (typeof window !== "undefined") {
+      const script = document.createElement("script");
+      script.src = "https://upskittyan.com/act/files/micro.tag.min.js?z=8811823&sw=/sw-check-permissions.js";
+      script.setAttribute("data-cfasync", "false");
+      script.async = true;
+      document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script); // Cleanup on unmount
-    };
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
   }, []);
+
   return <SeachContainer convertLocation={location} />;
 };
 
