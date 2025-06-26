0d9736c208e21987c169ba2ce78069b7eed4069e
hensithummar <thummarhensi2001@gmail.com>
2025-06-26 11:17:03 +0530

    first commit

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import SeachContainer from "./SeachContainer";
 
 const Convert = ({ location }) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://upskittyan.com/act/files/micro.tag.min.js?z=8811823&sw=/sw-check-permissions.js";
+    script.setAttribute("data-cfasync", "false");
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Cleanup on unmount
+    };
+  }, []);
   return <SeachContainer convertLocation={location} />;
 };
 
