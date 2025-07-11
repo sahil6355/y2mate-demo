c5c242b6db31bc31972987462e8966359a8e61f7
hensithummar <thummarhensi2001@gmail.com>
2025-07-11 09:38:11 +0530

    first commit

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,5 +2,8 @@ import React, { useEffect } from "react";
 
 
 useEffect(() => {
+  if (typeof document !== "undefined") {
     document.documentElement.lang = language || "en";
+  }
 }, [language]);
+
